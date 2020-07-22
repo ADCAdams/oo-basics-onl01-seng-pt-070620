849f1496c6f5418f9a69827dbb26ee3325ade74b
Andy Adams <andrewdcadams@gmail.com>
2020-07-22 13:24:50 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,20 @@
 
 
+class Book
+  attr_accessor :title :author :page_count
+  def initialize(title)
+   @title = title
+  end 
+  
+  def title
+    @title
+  end
+  
+  def author 
+    @author
+  end
+  def page_count
+    @page_count
+  end
+  
+end
