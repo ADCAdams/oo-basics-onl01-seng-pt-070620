e90dbbbfd5516e9456055f95a2466abdc84d2b8d
Andy Adams <andrewdcadams@gmail.com>
2020-07-22 13:29:38 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,7 +3,28 @@
 class Shoe 
   attr_accessor :brand, :color, :size, :material, :condition
   
-  def initialize
+  def initialize(brand)
+    @brand = brand 
+  end
+  
+  def color
+    @color
+  end
+  def size 
+    @size 
+  end
+  def material 
+    @material
+  end
+  def condition 
+    @condition
+  end
+  
+  def cobble 
+    @condition = "new"
+    puts "Shoe repaired"
+    
+  end
   
   
   
